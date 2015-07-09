5af1816c861a545c4ca6703758fc9fcd0a726801
Andrey Petrov <petrov.development@gmail.com>
2015-07-08 22:34:07 -0400

    testing. FIXME:the hamburger home menu button is not showing up

--- app/src/main/java/com/petrodevelopment/dice/HomeActivity.java
+++ app/src/main/java/com/petrodevelopment/dice/HomeActivity.java
@@ -2,16 +2,25 @@ package com.petrodevelopment.dice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
     }
 
     @Override
